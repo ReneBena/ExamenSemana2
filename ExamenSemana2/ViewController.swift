f56d16437383eb98aed8f2554994fbac09cb95cf
ARMONICO MAC <armonico@macbook-pro.local>
2016-09-14 00:39:23 -0600

    Initial Commit

---
+++ ExamenSemana2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExamenSemana2
+//
+//  Created by ARMONICO MAC  on 14/9/16.
+//  Copyright © 2016 ARMONICO MAC . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
